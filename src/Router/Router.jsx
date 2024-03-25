c569621b1cacb409c799580cdf14a4f860d40fa9
devenamulshikder <devenamulshikder@gmail.com>
2024-03-25 21:06:12 +0600

    banner added

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Error from "../Pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    errorElement:<Error></Error>,
+    element: <MainLayout></MainLayout>,
+    children:[
+        {
+            
+        }
+    ]
   },
 ]);
 export default router
